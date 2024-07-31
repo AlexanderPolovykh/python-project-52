ea4e50d181e9f5526acb080476811d02d81ca5ba
polovykh <polovykh@mail.ru>
2024-07-31 15:14:06 +0700

    step 3 (1)

--- task_manager/views.py
+++ task_manager/views.py
@@ -11,31 +11,45 @@ from django.views.generic.base import TemplateView
 from django.views.generic import ListView
 from task_manager.models import User
 
+navbar_context = {
+    "brand": _("Task Manager"),
+    "users": _("Users"),
+    "input": _("Sign In"),
+    "reg": _("Log In"),
+    "hello": _("Hello from Hexlet!"),
+    "about": _("Practical programming courses"),
+    "info": _("To learn more"),
+}
+
+user_list_context = {
+    "name": "Имя пользователя",
+    "full_name": "Полное имя",
+    "created_at": "Дата создания",
+}
+
 
 class MainPageView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context |= {
-            "brand": _("Task Manager"),
-            "users": _("Users"),
-            "input": _("Sign In"),
-            "reg": _("Log In"),
-            "hello": _("Hello from Hexlet!"),
-            "about": _("Practical programming courses"),
-            "info": _("To learn more"),
-        }
+        context |= navbar_context
         # messages.set_level(self.request, messages.WARNING)
         # messages.info(self.request, "Hello from Task Manager!")
         return context
 
 
 class UsersListPageView(ListView):
-    # model = User
-    queryset = User.objects.order_by('-created_at')
     template_name = "users.html"
+    # model = User
+    queryset = User.objects.order_by("-created_at")
     context_object_name = "user_list"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context |= navbar_context
+        context |= user_list_context
+        return context
 
 
 # class GetParametersMixin:
